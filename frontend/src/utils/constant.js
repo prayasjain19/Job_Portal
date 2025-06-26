5ed493b50c19f6495b0a2edef7fa937b5c63ebe6
prayasjain8765@gmail.com <prayasjain8765@gmail.com>
2025-06-26 17:28:18 +0530

    Changes Done in constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,4 @@
+//All The End Points
 export const USER_API_END_POINT="https://job-portal-mwzl.onrender.com/api/v1/user";
 export const JOB_API_END_POINT="https://job-portal-mwzl.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT="https://job-portal-mwzl.onrender.com/api/v1/application";
